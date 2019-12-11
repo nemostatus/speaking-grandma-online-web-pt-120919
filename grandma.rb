48fd0d14ed77ce3623e1684a15b7220763c382e1
Matthew Corralejo <mattcorralejo1@gmail.com>
2019-12-11 12:12:19 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -2,6 +2,7 @@ def speak_to_grandma(phrase)
 
 if phrase =="I LOVE YOU GRANDMA!"
   puts "I LOVE YOU TOO PUMPKIN!"
+elsif phrase == phrase.
 
 # If you shout, she can hear you (or at least she thinks so) 
 # and yells back
