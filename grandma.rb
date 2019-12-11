e8b8e5df1118e1992446a8786a19787be8e7de92
Matthew Corralejo <mattcorralejo1@gmail.com>
2019-12-11 12:18:53 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -12,10 +12,3 @@ else phrase == "WHAT?"
   puts "NO, NOT SINCE 1938!"
 
 
-# If you shout, she can hear you (or at least she thinks so) 
-# and yells back
-
-# NO, NOT SINCE 1938!
-
-# However if you say 'I LOVE YOU GRANDMA!', she should respond with
-# 'I LOVE YOU TOO PUMPKIN!'
