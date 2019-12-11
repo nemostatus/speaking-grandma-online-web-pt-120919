ac51918091cc1e37d517c7d7a6d06069dc81fb54
Matthew Corralejo <mattcorralejo1@gmail.com>
2019-12-11 12:14:07 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -2,7 +2,8 @@ def speak_to_grandma(phrase)
 
 if phrase =="I LOVE YOU GRANDMA!"
   puts "I LOVE YOU TOO PUMPKIN!"
-elsif phrase == 
+elsif phrase == "Hi Nana, how are you?"
+  puts
 
 # If you shout, she can hear you (or at least she thinks so) 
 # and yells back
